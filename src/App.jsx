b72d8acc7657c62d37d42f3b268a229a3245a230
M <116676788+Sudoku404@users.noreply.github.com>
2025-06-10 23:46:44 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Collection from './components/Meets';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Car Meets Around Me</h1>
+      <h2>Explore Different Communities</h2>
+      <Collection />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Meets.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Event from './Event'
+
+const Meets = () => {
+    return (
+        <div className = "Collection">
+            <table>
+                <tbody>
+                    <tr>
+                        <Event event ='Car Meet #1' img='@/src/Images/Meet#1.png' />
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+       
+    )
+}
+
+export default Meets;
